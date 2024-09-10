d10fcb3bb76401c990d0890d7506ad5dbfdb86aa
consuti-tec <consuti@consuti.com.br>
2024-09-09 21:24:30 -0300

    first commit

---
+++ src/AppConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ColorTheme: "#1f5123",
+  UrlPortal: "https://plataforma.sporti.com.br/cbru",
+  UrlPainel: "https://painel.sporti.com.br/",
+  UrlPortal2: "https://escoladoesporte.com.br/cbru",
+  UrlPainel2: "https://painel.escoladoesporte.com.br/",
+  TimeSplashScreen: 3000,
+};
